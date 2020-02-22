4cbb4f4da4e12e92d493becd474dd58d379fdf84
Davide Lugli <luglidavide.web@gmail.com>
2020-02-22 12:43:34 +0100

    snack-4

--- index.php
+++ index.php
@@ -1,22 +1,67 @@
-<!-- Stampare una stringa verde se la variabile password passata in GET è uguale a “Boolean”, altrimenti stampare una stringa rossa. -->
-<?php $psw = $_GET['password'];
-if ($psw == 'Boolean') {
-  $class = 'green';
-  $messaggio = 'Password Corretta. Benvenuto';
-} else {
-  $class = 'red';
-  $messaggio = 'Password errata. Riprova';
-}
-?>
+<!-- //Utilizzare questo array: https://pastebin.com/CkX3680A.
+    //Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+    <?php
 
+        $db = [
+            'teachers' => [
+                [
+                    'name' => 'Michele',
+                    'lastname' => 'Papagni'
+                ],
+                [
+                    'name' => 'Fabio',
+                    'lastname' => 'Forghieri'
+                ]
+            ],
+            'pm' => [
+                [
+                    'name' => 'Roberto',
+                    'lastname' => 'Marazzini'
+                ],
+                [
+                    'name' => 'Federico',
+                    'lastname' => 'Pellegrini'
+                ]
+            ]
+        ];
+
+    ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title>Snack 1 Params</title>
+    <title>Snack 4</title>
+    <style media="screen">
+      .green {
+        background-color: mediumspringgreen;
+        width: 100%;
+        height: 200px;
+      }
+      .gray {
+        background-color: silver;
+        width: 100%;
+        height: 200px;
+      }
+    </style>
   </head>
   <body>
-    <h1 class="<?php echo $class; ?>"><?php echo $messaggio ?></h1>
+  <div class="green">
+    <ul>
+<?php
+$pms = $db['pm'] ;
+for ($i=0; $i < count($pms) ; $i++) {
+echo '<li>'. $pms[$i]['name']. ' '.$pms[$i]['lastname']. '</li>';
+} ?>
+</ul>
+  </div>
+  <div class="gray">
+    <ul>
+    <?php
+    $teachers = $db['teachers'] ;
+    for ($i=0; $i < count($teachers) ; $i++) {
+    echo '<li>'. $teachers[$i]['name']. ' '.$teachers[$i]['lastname']. '</li>';
+    } ?>
+    </ul>
+  </div>
   </body>
 </html>
